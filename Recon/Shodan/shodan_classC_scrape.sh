66cebd54d4f9fd03895f518f5aea18d725fe4ddd
subinacls <admin@hacker-island.com>
2017-07-06 13:03:26 +0200

    Update shodan_classC_scrape.sh
    made dir structure to allow for different runs on different ranges

--- Recon/Shodan/shodan_classC_scrape.sh
+++ Recon/Shodan/shodan_classC_scrape.sh
@@ -1,13 +1,14 @@
 ip=$1;
+mkdir $ip 2&>/dev/null;
 for x in $(seq 1 255);do 
- y=`echo $ip`.`echo $x`_output; # sets output filename
+ y="./`echo $ip`/`echo $ip`.`echo $x`_output"; # sets output filename
  z=`echo $ip`.`echo $x`; # sets iterated IP address
  wget -O $y https://www.shodan.io/host/`echo $z`; # makes wget call and saves results to disk
  done; # finish the for loop
-find ./ -maxdepth 1 -size 0 -print0 | xargs -0 rm; # removes any file with size 0 (be careful)
+find ./`echo $ip` -maxdepth 1 -size 0 -print0 | xargs -0 rm; # removes any file with size 0 (be careful)
 # greps for specific line containing port numbers
 # removes Contact information for cleaner output
 # further cleaning of STDOUT for reading
-grep "<li><a href=\"#" ./* |\
+grep "<li><a href=\"#" ./`echo $ip`/* |\
 grep -v Contact |\
 sed -r "s/(.*)_output\:<li><a href=\"#[0-9]{1,5}\">(.*)<\/a>/\1:\2/g"
