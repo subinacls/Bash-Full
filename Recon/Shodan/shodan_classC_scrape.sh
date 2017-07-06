f5c3ec9a1dcefda18557cb2ca213bf1e838b587b
subinacls <admin@hacker-island.com>
2017-07-06 15:59:47 +0200

    Update shodan_classC_scrape.sh
    added warning clause and added usage examples

--- Recon/Shodan/shodan_classC_scrape.sh
+++ Recon/Shodan/shodan_classC_scrape.sh
@@ -1,17 +1,31 @@
+echo -e "[!] Class C Shodan Scraper\n  By: SubINaclS - 2017\n";
+################################################################################
+## Usage:
+##  Dump to STDOUT
+##    ./shodan_classC_scraper.sh 192.168.100 
+##  Save to file
+##    ./shodan_classC_scraper.sh 192.168.100 > saved_results.txt
+################################################################################
 ip=$1;
 mkdir $ip 2&>/dev/null;
-echo -e "[!] Class C Shodan Scraper\n  By: SubINaclS - 2017\n";
 for x in $(seq 1 255);do 
- y="./`echo $ip`/`echo $ip`.`echo $x`_output"; # sets output filename
- z=`echo $ip`.`echo $x`; # sets iterated IP address
+ # sets output filename
+ y="./`echo $ip`/`echo $ip`.`echo $x`_output";
+ # sets iterated IP address
+ z=`echo $ip`.`echo $x`;
  echo -e "\t[*] Scraping: `echo $z`";
- wget -O $y https://www.shodan.io/host/`echo $z` 2&>/dev/null; # makes wget call and saves results to disk
- done; # finish the for loop
-find ./`echo $ip` -maxdepth 1 -size 0 -print0 | xargs -0 rm; # removes any file with size 0 (be careful)
+ # makes wget call and saves results to disk
+ wget -O $y https://www.shodan.io/host/`echo $z` 2&>/dev/null; 
+ # finish the for loop
+ done;
+# removes any file with size 0 (be careful)
+find ./`echo $ip` -maxdepth 1 -size 0 -print0 | xargs -0 rm; 
+echo -e "\n[!] Finishing, Results as follows:";
 # greps for specific line containing port numbers
 # removes Contact information for cleaner output
 # further cleaning of STDOUT for reading
-echo -e "\n[!] Finishing, Results as follows:";
 grep "<li><a href=\"#" ./`echo $ip`/* |\
 grep -v Contact |\
 sed -r "s/(.*)_output\:<li><a href=\"#[0-9]{1,5}\">(.*)<\/a>/\1:\2/g"
+# Users be aware, using this script will delete files on your system
+################################################################################
