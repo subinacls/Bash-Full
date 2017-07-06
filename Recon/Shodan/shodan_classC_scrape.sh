5db4e05aebf6829cc87beb5ec59d96c7bfb05c0f
subinacls <admin@hacker-island.com>
2017-07-06 12:13:23 +0200

    Update shodan_classC_scrape.sh

--- Recon/Shodan/shodan_classC_scrape.sh
+++ Recon/Shodan/shodan_classC_scrape.sh
@@ -1,10 +1,13 @@
 ip=$1;
 for x in $(seq 1 255);do 
- y=`echo $ip`.`echo $x`_output;
- z=`echo $ip`.`echo $x`;
- wget -O $y https://www.shodan.io/host/`echo $z`;
- done;
-find ./ -size 0 -print0 | xargs -0 rm;
+ y=`echo $ip`.`echo $x`_output; # sets output filename
+ z=`echo $ip`.`echo $x`; # sets iterated IP address
+ wget -O $y https://www.shodan.io/host/`echo $z`; # makes wget call and saves results to disk
+ done; # finish the for loop
+find ./ -size 0 -print0 | xargs -0 rm; # removes any file with size 0 (be careful)
+# greps for specific line containing port numbers
+# removes Contact information for cleaner output
+# further cleaning of STDOUT for reading
 grep "<li><a href=\"#" ./* |\
 grep -v Contact |\
 sed -r "s/(.*)_output\:<li><a href=\"#[0-9]{1,5}\">(.*)<\/a>/\1:\2/g"
