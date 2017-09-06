29f45c583ad9b1e5476ddd1f38aa5ad1821cc016
subinacls <admin@hacker-island.com>
2017-09-05 23:43:26 -0600

    Create ssh_related.sh

---
+++ msf/scanners/ssh_related.sh
@@ -0,0 +1,18 @@
+of Service
+   auxiliary/fuzzers/ssh/ssh_kexinit_corrupt                                    normal     SSH Key Exchange Init Corruption
+   auxiliary/fuzzers/ssh/ssh_version_15                                         normal     SSH 1.5 Version Fuzzer
+   auxiliary/fuzzers/ssh/ssh_version_2                                          normal     SSH 2.0 Version Fuzzer
+   auxiliary/fuzzers/ssh/ssh_version_corrupt                                    normal     SSH Version Corruption
+   auxiliary/scanner/http/cisco_firepower_login                                 normal     Cisco Firepower Management Console 6.0 Login
+   auxiliary/scanner/http/gitlab_user_enum                     2014-11-21       normal     GitLab User Enumeration
+   auxiliary/scanner/ssh/apache_karaf_command_execution        2016-02-09       normal     Apache Karaf Default Credentials Command Execution
+   auxiliary/scanner/ssh/cerberus_sftp_enumusers               2014-05-27       normal     Cerberus FTP Server SFTP Username Enumeration
+   auxiliary/scanner/ssh/detect_kippo                                           normal     Kippo SSH Honeypot Detector
+   auxiliary/scanner/ssh/fortinet_backdoor                     2016-01-09       normal     Fortinet SSH Backdoor Scanner
+   auxiliary/scanner/ssh/juniper_backdoor                      2015-12-20       normal     Juniper SSH Backdoor Scanner
+   auxiliary/scanner/ssh/karaf_login                                            normal     Apache Karaf Login Utility
+   auxiliary/scanner/ssh/ssh_enumusers                                          normal     SSH Username Enumeration
+   auxiliary/scanner/ssh/ssh_identify_pubkeys                                   normal     SSH Public Key Acceptance Scanner
+   auxiliary/scanner/ssh/ssh_login                                              normal     SSH Login Check Scanner
+   auxiliary/scanner/ssh/ssh_login_pubkey                                       normal     SSH Public Key Login Scanner
+   auxiliary/scanner/ssh/ssh_version  
