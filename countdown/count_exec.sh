83bd7dc126258644c20ce1e7284110b84c281036
subinacls <admin@hacker-island.com>
2017-11-13 15:09:33 -0700

    Update count_exec.sh

--- countdown/count_exec.sh
+++ countdown/count_exec.sh
@@ -2,23 +2,23 @@
 # function to automatically run a command after a count down period
 #
 countdown() { 
- seconds=$1;
- date1=$((date +%s + $seconds));
- while [ "$date1" -ge date +%s ]; do
-  echo -ne "$(date -u --date @$(($date1 - date +%s )) +%H:%M:%S)\r";
- done;
- $2;
+ seconds=$1; 
+ date1=$((`date +%s` + $seconds)); 
+ while [ "$date1" -ge `date +%s` ]; do
+  echo -ne "$(date -u --date @$(($date1 - `date +%s` )) +%H:%M:%S)\r";
+ done ; 
+ echo; 
+ $2; 
 } ## countdown 1800 'systemctl stop ssh'
 
 # startup and countdown
 sshupanddown() { 
- seconds=$1;
- cmdup="systemctl start ssh"
- cmddown="systemctl stop ssh"
- $cmdup
- date1=$((date +%s + $seconds));
- while [ "$date1" -ge date +%s ]; do
-  echo -ne "$(date -u --date @$(($date1 - date +%s )) +%H:%M:%S)\r";
- done;
- $cmddown;
-} ## sshupanddown 1800
+ seconds=$1; 
+ systemctl start ssh;
+ date1=$((`date +%s` + $seconds)); 
+ while [ "$date1" -ge `date +%s` ]; do
+  echo -ne "$(date -u --date @$(($date1 - `date +%s` )) +%H:%M:%S)\r";
+ done ; 
+ echo; 
+ systemctl stop ssh; 
+} ## countdown 1800 'systemctl stop ssh'
