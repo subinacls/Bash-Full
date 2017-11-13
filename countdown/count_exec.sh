08873ad24069532f2f2792b898fae61311a627b3
subinacls <admin@hacker-island.com>
2017-11-13 12:49:56 -0700

    Update count_exec.sh

--- countdown/count_exec.sh
+++ countdown/count_exec.sh
@@ -9,3 +9,16 @@ countdown() {
  done;
  $2;
 } ## countdown 1800 'systemctl stop ssh'
+
+# startup and countdown
+sshupanddown() { 
+ seconds=$1;
+ cmdup="systemctl start ssh"
+ cmddown="systemctl stop ssh"
+ $cmdup
+ date1=$((date +%s + $seconds));
+ while [ "$date1" -ge date +%s ]; do
+  echo -ne "$(date -u --date @$(($date1 - date +%s )) +%H:%M:%S)\r";
+ done;
+ $cmddown;
+} ## sshupanddown 1800
