545f543dad6874835bc447181f7e8ad0e6094f21
subinacls <admin@hacker-island.com>
2018-07-10 14:20:57 -0600

    Update openssl_DNS.py

--- Validation/SSL/openssl_DNS.py
+++ Validation/SSL/openssl_DNS.py
@@ -1,33 +1,45 @@
-#!/usr/bin/env python
-#
-# takes a list of CIDR ranges and pulls the standard HTTPS port 443 to grab Domain names of the SSL Certificate installed / Used
-#
-# echo "1.2.3.0/24" > target_list
-# python openssl_DNS.py target_list
-
 import os
 from netaddr import *
 import sys
 import multiprocessing
+import __builtin__ as bi
 
-ips = list()
-with open(sys.argv[1]) as ipfile:
- for xip in ipfile:
-  ips.append(xip)
+try:
+ ips = list()
+ with open(sys.argv[1]) as ipfile:
+  for xip in ipfile:
+   ips.append(xip)
+except:
+ pass
 
 def openssl_process():
- cmd = 'echo | \
+ try:
+  cmd = 'echo | \
    timeout 2 openssl s_client -connect '+str(xnip)+':443 2>/dev/null | \
    openssl x509 -noout -text 2>/dev/null | \
    grep DNS: | \
    tr -d " \t" | \
    cut -d"." -f2-'
- print xnip, os.popen(cmd).readlines()
+  res = os.popen(cmd).readlines()
+  if res:
+   nlist = list()
+   reslist = [r.strip().replace('DNS:','') for r in res]
+   for xres in reslist:
+    for xs in xres.split(','):
+     nlist.append(xs)
+    for xso in sorted(set(nlist)):
+     print('{}\t{}'.format(xnip, xso))
+ except Exception as e:
+  print e
 
-jobs = []
-for nip in ips:
- for xnip in IPNetwork(nip):
-  p = multiprocessing.Process(target=openssl_process)
-  jobs.append(xnip)
-  p.start()
- p.join()
+
+try:
+ jobs = []
+ for nip in ips:
+  for xnip in IPNetwork(nip):
+   p = multiprocessing.Process(target=openssl_process)
+   jobs.append(xnip)
+   p.start()
+  p.join()
+except:
+ pass
